a78cbc70672b122af364b3636cabb7d290dc2c12
Pawan Dubey <git@pawandubey.com>
2022-06-18 18:18:03 -0400

    Initial commit

---
+++ lib/troml.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "troml/version"
+
+module Troml
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/troml/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Troml
+  VERSION = "0.1.0"
+end
